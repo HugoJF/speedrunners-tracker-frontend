8bc4aa9c4ac9fcfebc1db4ee424066bebc711e80
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2022-09-01 10:55:33 -0400

    fix: map select not triggering onChange event

--- app/components/map-select.js
+++ app/components/map-select.js
@@ -1,6 +1,13 @@
 import Component from '@glimmer/component';
 import { maps } from '../utils/maps';
+import {action} from "@ember/object";
+import {safeInvoke} from "../utils/functions";
 
 export default class MapSelectComponent extends Component {
   maps = maps;
+
+  @action
+  handleOnChange(e) {
+    safeInvoke(this.args.onChange, e.target.value);
+  }
 }
