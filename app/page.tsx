aa342616f286507723c545a21c2fe7fcef51a9a6
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2023-02-26 15:16:35 -0400

    chore: remove demo page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-'use client'
-
-import {Button} from "@/components/button";
-import {useState} from "react";
-import {Plus} from "react-feather";
-
-const text = 'Hello world';
+import {redirect} from "next/navigation";
 
 export default function Home() {
-    const [loading, setLoading] = useState(false)
+    redirect('/matches');
 
-    function handleOnClick() {
-        setLoading(true)
-
-        setTimeout(() => {
-            setLoading(false)
-        }, 2000)
-    }
-
-    return <div className="grid grid-cols-3 gap-4 text-green-500">
-        <Button icon={Plus} loading={loading} onClick={handleOnClick}>{text}</Button>
-        <Button loading>{text}</Button>
-        <Button>{text}</Button>
-
-        <div className="flex gap-4">
-            <Button loading={loading} onClick={handleOnClick}>{text}</Button>
-            <Button loading>{text}</Button>
-            <Button>{text}</Button>
-        </div>
-    </div>
+    return undefined;
 }
